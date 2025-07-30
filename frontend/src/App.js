95eeb4cd7161ec75ce4e6d6112403508328915f1
ElvisBrito98 <e.brito.eb88@gmail.com>
2025-07-30 15:52:44 -0100

    adicionei mapa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from './components/Header';
-
+import Header from './components/header';
+import Footer from './components/footer'; // 👈 Importa o Footer
 
 function App() {
   return (
@@ -11,6 +11,8 @@ function App() {
       <div className="ad-space">
         <p>Espaço reservado para futuras publicidades</p>
       </div>
+
+      <Footer /> {/* 👈 Chama o Footer */}
     </div>
   );
 }
---
+++ frontend/src/components/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="address">
+        Rua Dr.João Cesário Lacerda, nº 2, 7937-049<br />
+        Praia, Cabo Verde
+      </div>
+
+      <div className="rights">
+        Ⓒ 2025 Correios de Cabo Verde &nbsp;|&nbsp; Todos os Direitos Reservados
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
